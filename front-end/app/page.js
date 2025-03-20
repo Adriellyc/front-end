6acde3d00fbeabf7577b7e81a148b03eb33daa07
Adrielly.CS <165967593+Adriellyc@users.noreply.github.com>
2025-03-20 11:54:33 +0000

    a

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -2,80 +2,35 @@
 
 import { useState } from "react";
 
-// Componente para exibir a imagem do dado
-function Dado({ valor }) {
-  const imagens = {
-    1: "/dado1.png",
-    2: "/dado2.png",
-    3: "/dado3.png",
-    4: "/dado4.png",
-    5: "/dado5.png",
-    6: "/dado6.png",
-  };
-
-  return (
-    <div>
-      <img src={imagens[valor]} alt={`Dado mostrando ${valor}`} width={100} />
-    </div>
-  );
+function Texto1() {
+  return <p>Um outro parágrafo de texto!</p>;
 }
 
-// Função para gerar o número do dado aleatoriamente
-function gerarNumeroDado() {
-  return Math.floor(Math.random() * 6) + 1;
+function gerarNumAleat() {
+  return Math.floor(Math.random() * 100) + 1;
 }
 
 export default function Home() {
-  const [rodada, setRodada] = useState(1);
-  const [pontosJogador1, setPontosJogador1] = useState(0);
-  const [pontosJogador2, setPontosJogador2] = useState(0);
-  const [dadoJogador1, setDadoJogador1] = useState(1);
-  const [dadoJogador2, setDadoJogador2] = useState(1);
-  const [vencedor, setVencedor] = useState("");
-
-  // Função para jogar o dado e determinar o vencedor da rodada
-  function jogarRodada() {
-    const dado1 = gerarNumeroDado();
-    const dado2 = gerarNumeroDado();
-
-    setDadoJogador1(dado1);
-    setDadoJogador2(dado2);
-
-    if (dado1 > dado2) {
-      setPontosJogador1(pontosJogador1 + 1);
-    } else if (dado2 > dado1) {
-      setPontosJogador2(pontosJogador2 + 1);
-    }
-
-    if (rodada < 5) {
-      setRodada(rodada + 1);
-    } else {
-      if (pontosJogador1 > pontosJogador2) {
-        setVencedor("Jogador 1");
-      } else if (pontosJogador2 > pontosJogador1) {
-        setVencedor("Jogador 2");
-      } else {
-        setVencedor("Empate");
-      }
-    }
-  }
-
+  const [hide, setHide] = useState(false);
+  const [numAleat, setNumAleat] = useState(0);
+  const aula = 2;
   return (
     <div>
-      <h1>Jogo de Dados</h1>
-      <p>Rodada: {rodada}/5</p>
-      <div>
-        <h2>Jogador 1</h2>
-        <Dado valor={dadoJogador1} />
-        <p>Pontos: {pontosJogador1}</p>
-      </div>
-      <div>
-        <h2>Jogador 2</h2>
-        <Dado valor={dadoJogador2} />
-        <p>Pontos: {pontosJogador2}</p>
-      </div>
-      <button onClick={jogarRodada}>Jogar Rodada</button>
-      {vencedor && <h2>Vencedor: {vencedor}</h2>}
+      <h1>Olá Turma!!!!</h1>
+      <p>Essa é a nossa {aula}ª aula de React!</p>
+      <hr />
+      <button onClick={() => setNumAleat(gerarNumAleat())}>
+        Aletatório: {numAleat}
+      </button>
+      <hr />
+      <button onClick={() => setHide(!hide)}>{hide ? "Show" : "Hide"}</button>
+      <hr />
+      {!hide && (
+        <>
+          <Texto1 />
+          <Texto1 />
+        </>
+      )}
     </div>
   );
 }
