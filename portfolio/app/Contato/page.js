d64e14d7234b05add3c1274d03680be12f82cf98
Adrielly.CS <165967593+Adriellyc@users.noreply.github.com>
2025-05-28 01:27:08 +0000

    a

--- portfolio/app/Contato/page.js
+++ portfolio/app/Contato/page.js
@@ -2,7 +2,17 @@ export default function Contato() {
   return (
     <main>
       <h1>Contato</h1>
-      <p>Entre em contato comigo pelo GitHub: <a href="https://github.com/Adriellyc" target="_blank">Adriellyc</a></p>
+      <p>
+        Entre em contato comigo pelo GitHub:{" "}
+        <a 
+          href="https://github.com/Adriellyc" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          style={{ color: 'blue', textDecoration: 'underline' }} // deixa com cara de link
+        >
+          Adriellyc
+        </a>
+      </p>
     </main>
-  )
+  );
 }
