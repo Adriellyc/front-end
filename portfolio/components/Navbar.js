dd203ceccf590ab78c5177df48af500a6288bc7a
Adrielly.CS <165967593+Adriellyc@users.noreply.github.com>
2025-05-28 01:41:53 +0000

    a

--- portfolio/components/Navbar.js
+++ portfolio/components/Navbar.js
@@ -4,31 +4,11 @@ export default function Navbar() {
   return (
     <nav style={{ padding: '1rem', background: '#333' }}>
       <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none', margin: 0, padding: 0 }}>
-        <li>
-          <Link href="/" style={{ color: '#fff', textDecoration: 'none' }}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/sobre" style={{ color: '#fff', textDecoration: 'none' }}>
-            Sobre
-          </Link>
-        </li>
-        <li>
-          <Link href="/skills" style={{ color: '#fff', textDecoration: 'none' }}>
-            Skills
-          </Link>
-        </li>
-        <li>
-          <Link href="/projetos" style={{ color: '#fff', textDecoration: 'none' }}>
-            Projetos
-          </Link>
-        </li>
-        <li>
-          <Link href="/contato" style={{ color: '#fff', textDecoration: 'none' }}>
-            Contato
-          </Link>
-        </li>
+        <li><Link href="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link></li>
+        <li><Link href="/Sobre" style={{ color: '#fff', textDecoration: 'none' }}>Sobre</Link></li>
+        <li><Link href="/Skills" style={{ color: '#fff', textDecoration: 'none' }}>Skills</Link></li>
+        <li><Link href="/Projetos" style={{ color: '#fff', textDecoration: 'none' }}>Projetos</Link></li>
+        <li><Link href="/Contato" style={{ color: '#fff', textDecoration: 'none' }}>Contato</Link></li>
       </ul>
     </nav>
   );
