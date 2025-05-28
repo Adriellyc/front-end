fefb32c416c179915fc78e15fdc726c068f77d5e
Adrielly.CS <165967593+Adriellyc@users.noreply.github.com>
2025-05-28 02:42:10 +0000

    a

--- portfolio/app/layout.js
+++ portfolio/app/layout.js
@@ -1,18 +1,18 @@
-import './globals.css'
-import Navbar from '../components/Navbar'
+import './globals.css';
+import Navbar from '../components/Navbar';
 
 export const metadata = {
-  title: 'Portfólio Adrelly Campos',
+  title: 'Portfólio Adrielly Campos',
   description: 'Desenvolvedora Front End',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt">
+    <html lang="pt-BR">
       <body>
         <Navbar />
         {children}
       </body>
     </html>
-  )
+  );
 }
